b6a1dc14bf4e721db1c130ac0c26da9abcfeeb80
Fábio Sales <fabio.oliveira.sales@gmail.com>
2023-09-03 22:07:28 -0300

    refatorando o código

--- eletro-tech/src/main/java/br/com/fsales/eletrotech/pessoa/application/util/CycleAvoidingMappingContext.java
+++ eletro-tech/src/main/java/br/com/fsales/eletrotech/pessoa/application/util/CycleAvoidingMappingContext.java
@@ -8,7 +8,7 @@ import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class CycleAvoidingMappingContext {
-    private Map<Object, Object> knownInstances = new IdentityHashMap<Object, Object>();
+    private final Map<Object, Object> knownInstances = new IdentityHashMap<Object, Object>();
 
     @BeforeMapping
     public <T> T getMappedInstance(Object source, @TargetType Class<T> targetType) {
